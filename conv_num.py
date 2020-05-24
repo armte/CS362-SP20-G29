35717e752f2976514be3768a250ee75fcee44db5
Tommy Armstrong <armstrth@oregonstate.edu>
2020-05-23 21:17:23 -0700

    Made conv_num return the integer value of the string representation of a decimal number. Wrote get_int function and created a file for constants. It passed.

--- conv_num.py
+++ conv_num.py
@@ -1,2 +1,14 @@
+import constant
+
 def conv_num(str_num):
+    if len(str_num) > 0:
+        return get_int(str_num)
     return None
+
+def get_int(str_num):
+    int_num = 0
+    for i in range(len(str_num)):
+        digit = str_num[i]
+        int_num *= 10
+        int_num += (ord(digit) % constant.ASCII_0)
+    return int_num
