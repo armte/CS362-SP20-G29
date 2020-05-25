aad6271fb135f62915b2989ca723adb18b761dd0
Tommy Armstrong <armstrth@oregonstate.edu>
2020-05-24 19:30:20 -0700

    Made conv_num return the negative integer value of the string representation of a negative whole number. It passed

--- conv_num.py
+++ conv_num.py
@@ -27,24 +27,30 @@ def conv_num(str_num):
     return None
 
 
-def conv_int(str_num):
+def conv_int(int_match):
+    integral = int_match.group('int')
     int_num = 0
-    for digit in str_num:
+    for digit in integral:
         int_num *= 10
         int_num += (ord(digit) % constant.ASCII_0)
+    if int_match.group('sign') == '-':
+        int_num *= -1
     return int_num
 
 
-def conv_dec(int_frac):
-    frac_str = int_frac.group('frac')
+def conv_dec(dec_match):
+    frac_str = dec_match.group('frac')
     exp = -1
     fraction = 0.0
     for digit in frac_str:
         num = ord(digit) % constant.ASCII_0
         fraction += (num * 10**exp)
         exp -= 1
-    integral = int_frac.group('int')
-    dec_num = conv_int(integral) + fraction
+    dec_num = conv_int(dec_match)
+    if dec_num < 0:
+        dec_num -= fraction
+    else:
+        dec_num += fraction
     return dec_num
 
 
@@ -59,13 +65,15 @@ def conv_hex(hex_match):
             hex_val = hex_val.upper()
             num = ord(hex_val) % constant.ASCII_A10
         int_num += num
+    if hex_match.group('sign') == '-':
+        int_num *= -1
     return int_num
 
 
 def num_type(str_num):
-    # check if string is in proper decimal point format
-    # dec_check = re.split(r'\.', str_num)
-    dec_match = re.match(r'(?P<int>\d*)\.(?P<frac>\d*$)', str_num)
+    # check if string is in acceptable decimal point format
+    dec_match = re.match(r'(?P<sign>-*)(?P<int>\d*)\.(?P<frac>\d*$)',
+                         str_num, re.ASCII)
     if dec_match:
         # check that at least one side of decimal point is not empty
         empty = True
@@ -73,10 +81,13 @@ def num_type(str_num):
             if dec_val:
                 empty = False
         return (NumType.DEC, dec_match) if not empty else None
-    # check if string is in the proper hexadecimal format
-    hex_match = re.match(r'0x(?P<hvals>[0-9a-fA-F]+$)', str_num)
+    # check if string is in the acceptable hexadecimal format
+    hex_match = re.match(r'(?P<sign>-*)0x(?P<hvals>[0-9a-fA-F]+$)',
+                         str_num, re.ASCII)
     if hex_match:
         return NumType.HEX, hex_match
-    if str_num.isdecimal():
-        return NumType.INT, str_num
+    # check if string is in the acceptable integer format
+    int_match = re.match(r'(?P<sign>-*)(?P<int>\d+$)', str_num, re.ASCII)
+    if int_match:
+        return NumType.INT, int_match
     return None
