b8aec5028a8eafb5b76c2e9fb2387e4dfbe63889
Andre Pestovski <pestovsa@oregonstate.edu>
2020-05-25 22:51:09 -0700

    adding unittest file to my branch

--- tests.py
+++ tests.py
@@ -1,11 +1,26 @@
 import unittest
+from task import conv_endian
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        self.assertTrue(True)
+        self.assertEqual(conv_endian(954786),'0E 91 A2')
 
+    def test2(self):
+        self.assertEqual(conv_endian(-954786),'-0E 91 A2')
+
+    def test3(self):
+        self.assertEqual(conv_endian(954786, 'big'),'0E 91 A2')
+
+    def test4(self):
+        self.assertEqual(conv_endian(954786, 'little'),'A2 91 0E')
+
+    def test5(self):
+        self.assertEqual(conv_endian(954786, 'huge'),'None')
+
+    def test6(self):
+        self.assertEqual(conv_endian(954786, ' '),'None')
 
 if __name__ == "__main__":
     unittest.main()
