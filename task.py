524f799d5a82d06fbe4ae1a258a6c72d77061bc0
Andre Pestovski <pestovsa@oregonstate.edu>
2020-05-27 22:35:04 -0700

    moved mapped vals an dclean up

--- task.py
+++ task.py
@@ -1,13 +1,26 @@
-# map decimal nums to hex values
-mapped_vals = {
-    0: "0", 1: "1", 2: "2", 3: "3", 4: "4",
-    5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
-    10: "A", 11: "B", 12: "C", 13: "D",
-    14: "E", 15: "F",
-}
+"""
+Description: the function takes in an integer value as num and converts it
+to a hexadecimal number. The endian type is determined by the flag endian.
+The function will return the converted number as a string. It has the following
+specifications:
+*It may be assumed that num will always be an integer
+*Must be able to handle negative values for num
+*A value of big for endian will return a hexadecimal number that is big-endian
+*A value of little for endian will return a hexadecimal number that is
+little-endian
+*Any other values of endian will return None
+*The returned string will have each byte separated by a space
+*Each byte must be two characters in length"""
 
 
 def conv_endian(num, endian='big'):
+    # map decimal nums to hex values
+    mapped_vals = {
+        0: "0", 1: "1", 2: "2", 3: "3", 4: "4",
+        5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
+        10: "A", 11: "B", 12: "C", 13: "D",
+        14: "E", 15: "F",
+    }
     assert num == int(num)
     _hex = ""
     negative = False
@@ -18,30 +31,46 @@ def conv_endian(num, endian='big'):
         num *= -1
     while num > 0:
         # add condition for endian == 'big' or 'little, else return None
-        if endian == 'big':
+        if endian not in ['big', 'little']:
+            return None
+
+        while num > 0:
             num, remainder = num // 16, num % 16
             _hex = mapped_vals[remainder] + _hex
-        elif endian == 'little':
-            num, remainder = num // 16, num % 16
-            _hex = mapped_vals[remainder] + _hex
-            # reverse pairs of str for little endian
-            # _hex = ' '.join(reversed(_hex))
-            #
-            #reversedString = []
-            index = len(_hex)  # calculate length of string and save in index
-            while index > 0:
-                _hex += str[index - 1]  # save the value of str[index-1] in reverseString
-                index = index - 1  # decrement index
+
+        if len(_hex) % 2 == 1:
+            _hex = '0' + _hex
+
+        hex_string = ' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2))
+
+        if endian == 'little':
+            # reverse paired string of big endian to create little endian
+            hex_bytes = hex_string.split(' ')
+            little_endian = ' '.join(hex_bytes[::-1])
+            hex_string = little_endian
+            # add "-" for negative hex
+        if negative:
+            hex_string = '-' + hex_string
+
+        return hex_string
+
+        hex_string = ' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2))
+
+        return hex_string
+        while index > 0:
+            _hex += str[index - 1]
+            # save the value of str[index-1] in reverseString
+            index = index - 1  # decrement index
             print(_hex)  # reversed string
         else:
             return 'None'
-    # add "0" to hex to match requirements' format
-    _hex = '0' + _hex
-    if negative == True:
-        # add "-" to the hex value formatted in pair bytes
-        print("-" + ' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2)))
-        return "-" + ' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2))
-    if negative == False:
-        # otherwise return positive hex formatted in paired bytes
-        print(' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2)))
-        return ' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2))
+        # add "0" to hex to match requirements' format
+        _hex = '0' + _hex
+        if negative is True:
+            # add "-" to the hex value formatted in pair bytes
+            print("-" + ' '.join(_hex[i:i + 2]for i in range(0, len(_hex), 2)))
+            return "-" + ' '.join(_hex[i:i + 2]for i in range(0, len(_hex), 2))
+        if negative is False:
+            # otherwise return positive hex formatted in paired bytes
+            print(' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2)))
+            return ' '.join(_hex[i:i + 2] for i in range(0, len(_hex), 2))
--- tests.py
+++ tests.py
@@ -2,25 +2,29 @@ import unittest
 from task import conv_endian
 
 
-class TestCase(unittest.TestCase):
+class TestConvEndian(unittest.TestCase):
 
-    def test1(self):
-        self.assertEqual(conv_endian(954786),'0E 91 A2')
+    def test_endian1(self):
+        self.assertTrue(True)
+        self.assertEqual(conv_endian(954786), '0E 91 A2')
 
-    def test2(self):
-        self.assertEqual(conv_endian(-954786),'-0E 91 A2')
+    def test_endian2(self):
+        self.assertEqual(conv_endian(-954786), '-0E 91 A2')
 
-    def test3(self):
-        self.assertEqual(conv_endian(954786, 'big'),'0E 91 A2')
+    def test_endian3(self):
+        self.assertEqual(conv_endian(954786, 'big'), '0E 91 A2')
 
-    def test4(self):
-        self.assertEqual(conv_endian(954786, 'little'),'A2 91 0E')
+    def test_endian4(self):
+        self.assertEqual(conv_endian(954786, 'little'), 'A2 91 0E')
 
-    def test5(self):
-        self.assertEqual(conv_endian(954786, 'huge'),'None')
+    def test_test5(self):
+        self.assertEqual(conv_endian(954786, 'huge'), None)
 
-    def test6(self):
-        self.assertEqual(conv_endian(954786, ' '),'None')
+    def test_test6(self):
+        self.assertEqual(conv_endian(954786, ' '), None)
+
+    def test_test7(self):
+        self.assertEqual(conv_endian(-954786, 'big'), '-0E 91 A2')
 
 if __name__ == "__main__":
     unittest.main()
